1948e3fc88f524e71f2f939c84d6c3e31a3e4366
diegoaguilera4 <diegoaguilerareyes4@gmail.com>
2023-11-28 16:31:16 -0300

    Se agrega el paciente nuevo al arreglo de pacientes del profesional

--- lib/pages/agregarPaciente.dart
+++ lib/pages/agregarPaciente.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:gestiontareas/colores.dart';
 import 'package:http/http.dart' as http;
+import 'package:jwt_decoder/jwt_decoder.dart';
 
 class AgregarPacienteView extends StatefulWidget {
   final String token;
@@ -17,13 +18,63 @@ class _AgregarPacienteViewState extends State<AgregarPacienteView> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController nombreController = TextEditingController();
   final TextEditingController rutController = TextEditingController();
+  String? idProfesional;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeEmail();
+  }
+
+  // Función para inicializar el campo 'email'
+  void _initializeEmail() {
+    try {
+      Map<String, dynamic> jwtDecodedToken = JwtDecoder.decode(widget.token);
+      idProfesional = jwtDecodedToken['usuarioId'];
+    } catch (e) {
+      // Manejar cualquier error al decodificar el token, por ejemplo, token no válido.
+      print('Error al decodificar el token: $e');
+      // Puedes manejar el error de otra manera, como cerrar sesión y volver a la pantalla de inicio.
+    }
+  }
 
   Future<void> _crearPaciente(Map<String, String> pacienteData) async {
-    if (nombreController.text.isNotEmpty && rutController.text.isNotEmpty) {
-      var url = Uri.parse('http://localhost:3000/paciente/add');
-      var response = await http.post(url,
+    try {
+      if (nombreController.text.isNotEmpty && rutController.text.isNotEmpty) {
+        var url = Uri.parse('http://localhost:3000/paciente/add');
+        var response = await http.post(
+          url,
           headers: {"Content-Type": "application/json"},
-          body: jsonEncode(pacienteData));
+          body: jsonEncode(pacienteData),
+        );
+
+        if (response.statusCode == 201) {
+          // Cambiado a 201
+          var pacienteId = jsonDecode(response.body)['_id'];
+
+          var url2 = Uri.parse(
+              'http://localhost:3000/usuario/addPaciente/$idProfesional');
+          var response2 = await http.post(
+            url2,
+            headers: {"Content-Type": "application/json"},
+            body: jsonEncode({'pacienteId': pacienteId}),
+          );
+
+          if (response2.statusCode == 200) {
+            // Éxito en ambas solicitudes
+            print('Ambas solicitudes completadas con éxito');
+          } else {
+            // Manejar error en la segunda solicitud
+            print('Error en la segunda solicitud: ${response2.statusCode}');
+          }
+        } else {
+          // Manejar error en la primera solicitud
+          print('Error en la primera solicitud: ${response.statusCode}');
+        }
+      }
+    } catch (error) {
+      // Manejar errores generales
+      print('Error: $error');
     }
   }
 
